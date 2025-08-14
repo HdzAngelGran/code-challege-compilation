90690659f97b2316af421a76e25c3ff9ef502c1e
arkn37 <arkn37.dev@outlook.com>
2025-08-14 17:19:52 -0600

    refactor: lower case whole phase and then camel case reverseword

--- java/reverseword/Main.java
+++ java/reverseword/Main.java
@@ -3,13 +3,13 @@ package reverseword;
 public class Main {
 
   private static void reverseWord(String string) {
-    String[] words = string.split("\\s");
+    String[] words = string.toLowerCase().split("\\s");
     StringBuilder reversedString = new StringBuilder();
 
     for (int i = words.length - 1; i >= 0; i--) {
       char[] chars = words[i].toCharArray();
       for (int j = chars.length - 1; j >= 0; j--) {
-        reversedString.append(chars[j]);
+        reversedString.append(j == chars.length - 1 ? Character.toUpperCase(chars[j]) : chars[j]);
       }
       reversedString.append(" ");
     }
